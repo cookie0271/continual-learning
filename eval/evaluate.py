9480b8bb791203cf450fd6a23566edadfed0bc25
f611 <wzj651123@163.com>
2026-01-29 11:26:25 +0800

    test when task finish

--- eval/evaluate.py
+++ eval/evaluate.py
@@ -76,7 +76,7 @@ def test_acc(model, dataset, batch_size=128, test_size=1024, verbose=True, conte
 
 
 def test_all_so_far(model, datasets, current_context, iteration, test_size=None, no_context_mask=False,
-                    visdom=None, summary_graph=True, plotting_dict=None, verbose=False):
+                    visdom=None, summary_graph=True, plotting_dict=None, verbose=False, return_all=False):
     '''Evaluate accuracy of a classifier (=[model]) on all contexts so far (= up to [current_context]) using [datasets].
 
     [visdom]      None or <dict> with name of "graph" and "env" (if None, no visdom-plots are made)'''
@@ -124,7 +124,8 @@ def test_all_so_far(model, datasets, current_context, iteration, test_size=None,
                 [average_precs], names=["ave"], title="ave accuracy ({})".format(visdom["graph"]),
                 iteration=iteration, env=visdom["env"], ylabel="test accuracy"
             )
-
+    if return_all:
+        return precs, average_precs
 
 def initiate_plotting_dict(n_contexts):
     '''Initiate <dict> with accuracy-measures to keep track of for plotting.'''
